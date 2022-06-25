bd13acfb4bc71a535e9353fc9502d02765d3a68a
Matheus <mathspa98@gmail.com>
2022-06-25 15:26:53 -0300

    :truck: renamed GeneralCard component

--- src/components/GeneralCard.tsx
+++ src/components/GeneralCard.tsx
@@ -1,7 +1,7 @@
 import { Flex } from '@chakra-ui/react';
 import React from 'react';
 
-export default function GeneralCards() {
+export default function GeneralCard() {
   return (
     <Flex>
       data go here
