b9971691188f151ba48f217e367c6eb19ffa656d
Matheus <mathspa98@gmail.com>
2022-06-25 18:37:07 -0300

    :bricks: added GeneralCardInterface for respective props

--- src/services/interfaces/cardHeaderInterface.ts
+++ src/services/interfaces/cardHeaderInterface.ts
@@ -2,6 +2,7 @@ interface CardHeaderInterface {
   icon: string;
   title: string;
   options: string[];
+  select: boolean;
 }
 
 export default CardHeaderInterface;
---
+++ src/services/interfaces/generalCardInterface.ts
@@ -0,0 +1,9 @@
+import CardHeaderInterface from './cardHeaderInterface';
+import CardLinkInterface from './cardLinkInterface';
+
+interface GeneralCardInterface {
+  header: CardHeaderInterface;
+  link: CardLinkInterface;
+  type: string;
+}
+export default GeneralCardInterface;
