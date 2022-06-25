0253db70beb95ee688e4f4aaa0034d3d9771c493
Matheus <mathspa98@gmail.com>
2022-06-25 19:49:55 -0300

    :art: implemented header and container to main page

--- src/pages/CardsTemplate.tsx
+++ src/pages/CardsTemplate.tsx
@@ -1,18 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PageHeader from '../components/PageHeader';
 import CardsContainer from '../components/CardsContainer';
 
 export default function CardsTemplate() {
-  const [isLoading, setIsLoading] = React.useState({ matheus: 'valor inicial' });
-
-  React.useEffect(() => {
-    fetch('/api/users')
-      .then((response) => response.json())
-      .then((data) => setIsLoading(data));
-  }, []);
   return (
     <div>
       <PageHeader />
+      <CardsContainer />
     </div>
   );
 }
