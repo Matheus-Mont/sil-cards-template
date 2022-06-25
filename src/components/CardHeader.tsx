e62645777895f86b3efcfd6dd2bbf0daed99f612
Matheus <mathspa98@gmail.com>
2022-06-25 18:39:40 -0300

    :art: conditional rendering to CardHeader's select

--- src/components/CardHeader.tsx
+++ src/components/CardHeader.tsx
@@ -3,16 +3,20 @@ import { Flex, Select, Text } from '@chakra-ui/react';
 import CardHeaderIcon from './CardHeaderIcon';
 import CardHeaderInterface from '../services/interfaces/cardHeaderInterface';
 
-export default function CardHeader({ icon, title, options }: CardHeaderInterface) {
+export default function CardHeader({
+  icon, title, options, select,
+}: CardHeaderInterface) {
   return (
     <Flex>
       <CardHeaderIcon icon={icon} />
       <Text>{title}</Text>
+      {select && (
       <Select>
         {options.map((option) => (
           <option key={option} value={option.toLocaleLowerCase()}>{option}</option>
         ))}
       </Select>
+      )}
     </Flex>
   );
 }
