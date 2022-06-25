91352efe1d14ac3196cac1b370ff2a0c0fc262c8
Matheus <mathspa98@gmail.com>
2022-06-25 15:24:55 -0300

    :sparkles: added components CardContainer and GeneralCard

---
+++ src/components/CardsContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function CardsContainer() {
+  return (
+    <section>
+      cards go here
+    </section>
+  );
+}
---
+++ src/components/GeneralCard.tsx
@@ -0,0 +1,10 @@
+import { Flex } from '@chakra-ui/react';
+import React from 'react';
+
+export default function GeneralCards() {
+  return (
+    <Flex>
+      data go here
+    </Flex>
+  );
+}
