422872350d87703a2a3375376f2c33e3d9b79766
Matheus <mathspa98@gmail.com>
2022-06-25 19:47:21 -0300

    :sparkles: insert the 3 main cards to container

--- src/components/CardsContainer.tsx
+++ src/components/CardsContainer.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import GeneralCard from './GeneralCard';
+import SpecialCard from './SpecialCard';
 
 export default function CardsContainer() {
   return (
     <section>
-      cards go here
+      <GeneralCard
+        header={{
+          icon: 'eye', title: 'Confirmed cases', options: ['RJ', 'SP', 'MG'], select: true,
+        }}
+        link={{ text: 'Learn how to prevent infections ', href: 'https://www.health.harvard.edu/staying-healthy/how-to-prevent-infections' }}
+        type="cases"
+      />
+      <GeneralCard
+        header={{
+          icon: 'list', title: 'Top posts', options: ['Wallstreetbets', 'Bovespa'], select: true,
+        }}
+        link={{ text: 'Do you want more visits? Contact us ', href: 'https://silrs.com.br/' }}
+        type="posts"
+      />
+      <SpecialCard
+        link={{ text: 'Do you want more visits? Contact us ', href: 'https://silrs.com.br/' }}
+      />
     </section>
   );
 }
