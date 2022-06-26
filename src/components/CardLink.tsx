be5e8fbbcd01534f9d84d90584bb2b5907bcecfb
Matheus <mathspa98@gmail.com>
2022-06-26 17:56:09 -0300

    :lipstick: adding style to link[

--- src/components/CardLink.tsx
+++ src/components/CardLink.tsx
@@ -1,4 +1,4 @@
-import { Link } from '@chakra-ui/react';
+import { Flex, Link, Text } from '@chakra-ui/react';
 import React from 'react';
 import { HiArrowRight } from 'react-icons/hi';
 import CardLinkInterface from '../services/interfaces/cardLinkInterface';
@@ -6,8 +6,12 @@ import CardLinkInterface from '../services/interfaces/cardLinkInterface';
 export default function CardLink({ text, href }: CardLinkInterface) {
   return (
     <Link href={href}>
-      {text}
-      <HiArrowRight />
+      <Flex alignItems="center" as="u" color="#90CDF4" fontWeight="bold">
+        <Text mr="1">
+          {text}
+        </Text>
+        <HiArrowRight />
+      </Flex>
     </Link>
 
   );
