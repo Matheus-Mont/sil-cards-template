6ae077435eca6db1ff6245d35ce48f86a751a187
Matheus <mathspa98@gmail.com>
2022-06-26 17:29:30 -0300

    :lipstick: added style to CardHeader (select and text)

--- src/components/CardHeader.tsx
+++ src/components/CardHeader.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Flex, Select, Text } from '@chakra-ui/react';
+import {
+  Center, Flex, Select, Text,
+} from '@chakra-ui/react';
 import CardHeaderIcon from './CardHeaderIcon';
 import CardHeaderInterface from '../services/interfaces/cardHeaderInterface';
 
@@ -7,15 +9,19 @@ export default function CardHeader({
   icon, title, options, select, newSearch,
 }: CardHeaderInterface) {
   return (
-    <Flex>
-      <CardHeaderIcon icon={icon} />
-      <Text>{title}</Text>
+    <Flex justifyContent="space-between">
+      <Flex alignItems="center">
+        <CardHeaderIcon icon={icon} />
+        <Text mx={4} color="#1a365d" fontSize="lg" fontWeight="800">{title}</Text>
+      </Flex>
       {select && (
-      <Select onChange={newSearch}>
-        {options.map((option) => (
-          <option key={option} value={option.toLocaleLowerCase()}>{option}</option>
-        ))}
-      </Select>
+      <Center>
+        <Select variant="unstyled" color="gray.500" fontWeight="bold" fontSize="sm" size="lg" onChange={newSearch}>
+          {options.map((option) => (
+            <option key={option} value={option.toLocaleLowerCase()}>{option}</option>
+          ))}
+        </Select>
+      </Center>
       )}
     </Flex>
   );
