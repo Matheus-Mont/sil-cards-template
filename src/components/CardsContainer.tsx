ac76bde9315963c4543735979b5d0c05540db684
Matheus <mathspa98@gmail.com>
2022-06-26 16:30:28 -0300

    :lipstick: added flex to card container

--- src/components/CardsContainer.tsx
+++ src/components/CardsContainer.tsx
@@ -1,3 +1,4 @@
+import { Flex } from '@chakra-ui/react';
 import React from 'react';
 import GeneralCard from './GeneralCard';
 import SpecialCard from './SpecialCard';
@@ -6,23 +7,26 @@ import TableCard from './TableCard';
 export default function CardsContainer() {
   return (
     <section>
-      <GeneralCard
-        header={{
-          icon: 'eye', title: 'Confirmed cases', options: ['RJ', 'SP', 'MG'], select: true,
-        }}
-        link={{ text: 'Learn how to prevent infections ', href: 'https://www.health.harvard.edu/staying-healthy/how-to-prevent-infections' }}
-        type="cases"
-      />
-      <TableCard
-        header={{
-          icon: 'list', title: 'Top posts', options: ['Wallstreetbets', 'Bovespa'], select: true,
-        }}
-        link={{ text: 'Do you want more visits? Contact us ', href: 'https://silrs.com.br/' }}
-        type="posts"
-      />
-      <SpecialCard
-        link={{ text: 'Do you want more visits? Contact us ', href: 'https://silrs.com.br/' }}
-      />
+      <Flex>
+
+        <GeneralCard
+          header={{
+            icon: 'eye', title: 'Confirmed cases', options: ['RJ', 'SP', 'MG'], select: true,
+          }}
+          link={{ text: 'Learn how to prevent infections ', href: 'https://www.health.harvard.edu/staying-healthy/how-to-prevent-infections' }}
+          type="cases"
+        />
+        <TableCard
+          header={{
+            icon: 'list', title: 'Top posts', options: ['Wallstreetbets', 'Bovespa'], select: true,
+          }}
+          link={{ text: 'Do you want more visits? Contact us ', href: 'https://silrs.com.br/' }}
+          type="posts"
+        />
+        <SpecialCard
+          link={{ text: 'Do you want more visits? Contact us ', href: 'https://silrs.com.br/' }}
+        />
+      </Flex>
     </section>
   );
 }
