bd3be56c7cead8285447a1d4bfc7c7c9fe9238ae
Matheus <mathspa98@gmail.com>
2022-06-26 11:17:16 -0300

    :label: updated mainContentInterface

--- src/services/interfaces/mainContentInterface.ts
+++ src/services/interfaces/mainContentInterface.ts
@@ -1,7 +1,9 @@
+import React from 'react';
 import PostsInterface from './postsInterface';
 
 interface MainContentInterface {
   content: string | Array<PostsInterface>;
+  newSearch: (event: React.ChangeEvent<HTMLSelectElement>) => void;
 }
 
 export default MainContentInterface;
