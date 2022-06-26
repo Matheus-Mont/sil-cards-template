ce7602e7802972632252b980ae5e2f37743b105b
Matheus <mathspa98@gmail.com>
2022-06-26 12:07:38 -0300

    :fire: removed unecessary import

--- src/components/GeneralCardMainContent.tsx
+++ src/components/GeneralCardMainContent.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Text } from '@chakra-ui/react';
-import CardPostTable from './CardPostTable';
 import MainContentInterface from '../services/interfaces/mainContentInterface';
 
 export default function GeneralCardMainContent({ content }: MainContentInterface) {
