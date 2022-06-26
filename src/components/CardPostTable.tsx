af1b72e6e405ea6da07bf2d0fc2d39554fc8a2b9
Matheus <mathspa98@gmail.com>
2022-06-26 12:36:11 -0300

    :ambulance: changed props name

--- src/components/CardPostTable.tsx
+++ src/components/CardPostTable.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import TableInterface from '../services/interfaces/tableInterface';
 
-export default function CardPostTable({ posts }: TableInterface) {
+export default function CardPostTable({ content }: TableInterface) {
   return (
     <div>
       <table>
         <tbody>
-          {posts.map((post) => (
+          {content.map((post) => (
             <tr key={post.views}>
               <td>{post.postTitle}</td>
               <td>{post.views}</td>
