793572b59e72470bb14a5544ba530c687461f17a
Matheus <mathspa98@gmail.com>
2022-06-26 17:28:47 -0300

    :lipstick: changed incon's size

--- src/components/CardHeaderIcon.tsx
+++ src/components/CardHeaderIcon.tsx
@@ -3,5 +3,5 @@ import { AiOutlineEye, AiOutlineUnorderedList } from 'react-icons/ai';
 import IconInterface from '../services/interfaces/iconInterface';
 
 export default function CardHeaderIcon({ icon }: IconInterface) {
-  return icon === 'eye' ? <AiOutlineEye size="6vh" /> : <AiOutlineUnorderedList size="6vh" />;
+  return icon === 'eye' ? <AiOutlineEye size="4vh" /> : <AiOutlineUnorderedList size="4vh" />;
 }
