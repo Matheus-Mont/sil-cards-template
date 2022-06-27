afb83199f7e5f58712e44df28db54a69b83deb21
Matheus <mathspa98@gmail.com>
2022-06-27 14:22:20 -0300

    :lipstick: adjusting positioning

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,14 +1,17 @@
-import { Spinner } from '@chakra-ui/react';
+import { Flex, Spinner } from '@chakra-ui/react';
 import React from 'react';
 
 export default function Loader() {
   return (
-    <Spinner
-      thickness="4px"
-      speed="0.65s"
-      emptyColor="gray.200"
-      color="blue.500"
-      size="xl"
-    />
+    <Flex grow="1" justifyContent="center" alignItems="center">
+
+      <Spinner
+        thickness="4px"
+        speed="0.65s"
+        emptyColor="gray.200"
+        color="blue.500"
+        size="xl"
+      />
+    </Flex>
   );
 }
