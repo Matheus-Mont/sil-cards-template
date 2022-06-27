c4ed6982086b347de84f9dfe7d180c2513777c3c
Matheus <mathspa98@gmail.com>
2022-06-27 14:13:53 -0300

    :iphone: turning CardsContainer's style responsive)

--- src/components/CardsContainer.tsx
+++ src/components/CardsContainer.tsx
@@ -6,40 +6,69 @@ import TableCard from './TableCard';
 
 export default function CardsContainer() {
   return (
-    <section>
-      <Flex
-        pos="absolute"
-        top="15vh"
-        left={['3vh', '3vh', '20vh', '24vh']}
-        right={['3vh', '3vh', '20vh', '24vh']}
-        flexWrap={['wrap', 'wrap', 'wrap', 'nowrap']}
-        maxWidth="80%"
-      >
+    <Flex
+      pos={{
+        base: 'absolute', md: 'absolute', lg: 'absolute', xl: 'absolute',
+      }}
+      top="15vh"
+      // w={['100%', '100%', '100%', '100%']}
+      flexDirection={
+        {
+          base: 'column', md: 'row', lg: 'row', xl: 'row',
+        }
+}
+      justifyContent={
+        {
+          lg: 'flex-start', xl: 'flex-start',
+        }
+}
+      alignItems={
+        {
+          base: 'center', md: 'center', lg: 'flex-start', xl: 'flex-start',
+        }
+}
+      left={
+        {
+          base: '2vh', md: '9vh', lg: '9vh', xl: '24vh',
+        }
+}
+      right={
+        {
+          base: '0', md: '9vh', lg: '9vh', xl: '24vh',
+        }
+}
+      flexWrap={
+        {
+          base: 'wrap', md: 'wrap', lg: 'wrap', xl: 'nowrap',
+        }
+      }
+      mr="0"
+      maxWidth="90%"
+    >
 
-        <GeneralCard
-          header={{
-            icon: 'eye', title: 'Confirmed cases', options: ['RJ', 'SP', 'MG'], select: true,
-          }}
-          link={{
-            text: 'Learn how to prevent infections ', href: 'https://www.health.harvard.edu/staying-healthy/how-to-prevent-infections', color: '#90CDF4', arrow: true,
-          }}
-          type="cases"
-        />
-        <TableCard
-          header={{
-            icon: 'list', title: 'Top posts', options: ['Wallstreetbets', 'Bovespa'], select: true,
-          }}
-          link={{
-            text: 'Do you want more visits? Contact us ', href: 'https://silrs.com.br/', color: '#90CDF4', arrow: false,
-          }}
-          type="posts"
-        />
-        <SpecialCard
-          link={{
-            text: 'Write a review on TrustPilot ', href: 'https://silrs.com.br/', color: '#90CDF4', arrow: true,
-          }}
-        />
-      </Flex>
-    </section>
+      <GeneralCard
+        header={{
+          icon: 'eye', title: 'Confirmed cases', options: ['RJ', 'SP', 'MG'], select: true,
+        }}
+        link={{
+          text: 'Learn how to prevent infections ', href: 'https://www.health.harvard.edu/staying-healthy/how-to-prevent-infections', color: '#90CDF4', arrow: true,
+        }}
+        type="cases"
+      />
+      <TableCard
+        header={{
+          icon: 'list', title: 'Top posts', options: ['Wallstreetbets', 'Bovespa'], select: true,
+        }}
+        link={{
+          text: 'Do you want more visits? Contact us ', href: 'https://silrs.com.br/', color: '#90CDF4', arrow: false,
+        }}
+        type="posts"
+      />
+      <SpecialCard
+        link={{
+          text: 'Write a review on TrustPilot ', href: 'https://silrs.com.br/', color: '#90CDF4', arrow: true,
+        }}
+      />
+    </Flex>
   );
 }
