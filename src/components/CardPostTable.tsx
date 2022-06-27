1558cd721875a8ac6d2f94fc31a00268bcb3e84e
Matheus <mathspa98@gmail.com>
2022-06-27 10:50:36 -0300

    :art: changed structure from table to flex and styled

--- src/components/CardPostTable.tsx
+++ src/components/CardPostTable.tsx
@@ -1,19 +1,19 @@
+import {
+  Flex,
+  Table, Tbody, Td, Text, Tr,
+} from '@chakra-ui/react';
 import React from 'react';
 import TableInterface from '../services/interfaces/tableInterface';
 
 export default function CardPostTable({ content }: TableInterface) {
   return (
-    <div>
-      <table>
-        <tbody>
-          {content.map((post) => (
-            <tr key={post.views}>
-              <td>{post.postTitle}</td>
-              <td>{post.views}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <Flex direction="column" grow="1" justifyContent="center">
+      {content.map((post) => (
+        <Flex key={post.views} justifyContent="space-between">
+          <Text color="#718096" fontWeight="bold" fontSize="small" maxW="70%">{post.postTitle}</Text>
+          <Text color="#1a365d" fontWeight="bold">{post.views}</Text>
+        </Flex>
+      ))}
+    </Flex>
   );
 }
