85d2f0395bec5cc6aa76bb60213fc450af08aa37
Matheus <mathspa98@gmail.com>
2022-06-27 09:19:25 -0300

    :lipstick: added style to main content number

--- src/components/GeneralCardMainContent.tsx
+++ src/components/GeneralCardMainContent.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Text } from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
 import MainContentInterface from '../services/interfaces/mainContentInterface';
 
 export default function GeneralCardMainContent({ content }: MainContentInterface) {
   return (
-    <div>
-      <Text>{content.cases}</Text>
-    </div>
+    <Flex flexGrow="1" alignItems="center">
+      <Text fontSize="4xl" color="#1a365d" fontWeight="normal">{content.cases}</Text>
+    </Flex>
   );
 }
