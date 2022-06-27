8f58d07c192a8de10e82f4626dc8e1561b18df4c
Matheus <mathspa98@gmail.com>
2022-06-27 11:05:31 -0300

    :lipstick: added style to error component

--- src/components/ErrorScreen.tsx
+++ src/components/ErrorScreen.tsx
@@ -9,10 +9,10 @@ interface ErrorInterface {
 
 export default function ErrorScreen({ value, newSearch }: ErrorInterface) {
   return (
-    <Flex>
-      <RiErrorWarningFill />
-      <Text>Request failed!</Text>
-      <Button name={value} onClick={newSearch}>Reload</Button>
+    <Flex grow="1" alignItems="center" justifyContent="space-around" bg="#FED7D7" borderRadius="10px" p="2">
+      <RiErrorWarningFill size="6vh" color="#E53E3E" />
+      <Text color="#1a365d" fontWeight="bold">Request failed!</Text>
+      <Button boxShadow="2xl" name={value} onClick={newSearch}>Reload</Button>
     </Flex>
   );
 }
