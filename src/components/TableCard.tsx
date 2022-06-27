4678bbc5014a415e139d3fe567cf5bdd1bba0b3f
Matheus <mathspa98@gmail.com>
2022-06-27 14:30:26 -0300

    :rotating_light: removed unecessary import

--- src/components/TableCard.tsx
+++ src/components/TableCard.tsx
@@ -1,4 +1,4 @@
-import { Flex, Text } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
 import React, { useState, useEffect } from 'react';
 import CardHeader from './CardHeader';
 import CardLink from './CardLink';
